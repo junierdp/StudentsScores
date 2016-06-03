015c61e6c69b59168eb8ac40029a1f8c02161eb5
Junier Damian <onieldp@gmail.com>
2016-06-02 20:03:43 -0400

    Rename the 'save' method to 'insert'.

--- src/Interfaces/GenericDAO.java
+++ src/Interfaces/GenericDAO.java
@@ -4,7 +4,7 @@ package Interfaces;
 import java.util.List;
 
 public interface GenericDAO<M> {
-    public void save(M m);
+    public void insert(M m);
     public List<M> search(int id);
     public List<M> showAll();
     public void delete(M m);
