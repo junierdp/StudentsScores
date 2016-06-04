79ff8262dff4dbc9f728bbdb867ec2a1b8d6bd05
Junier Damian <onieldp@gmail.com>
2016-06-04 18:15:04 -0400

    Adding the controller ScoreDAOImpl and implementing the insert method.

---
+++ src/Controller/ScoreDAOImpl.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import Interfaces.ScoreDAO;
+import Model.Score;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class ScoreDAOImpl implements ScoreDAO {
+    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("StudentsScoresPU");
+    EntityManager em;
+    Query q;
+
+
+    @Override
+    public void insert(Score s) {
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(s);
+        em.getTransaction().commit();
+    }
+
+    @Override
+    public List<Score> search(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Score> showAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Score m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Score m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
